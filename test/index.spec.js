736aa0352b4d3c6dee42ea2115f1ee784e4824fc
Michael Rubanov <mrubanov@gmail.com>
2017-11-26 00:48:02 +0200

    @wip @restructure

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,24 +5,3 @@ describe('"Core" module exports', () => {
     expect(Core).to.be.instanceOf(Object)
   })
 })
-describe('`transform-object-rest-spread` babel plugin usage example.', function () {
-  let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }
-  it('should spread rest from destructed object.', function () {
-    expect(x).to.be.eq(1)
-    expect(y).to.be.eq(2)
-    expect(z).to.be.eql({ a: 3, b: 4 })
-  })
-  let a = 99
-  let b = 99
-  let j = 9
-  let n = { j, x, y, a, b, ...z }
-  it('should merge with spread rest object while overriding existing values.', function () {
-    let expected = { x: 1, y: 2, a: 3, b: 4, j }
-    expect(n).to.be.eql(expected)
-  })
-  let m = {...z, a: 99, z: 1}
-  it('should merge with spread rest object while NOT overriding existing values, in case placed as first property of produced object.', function () {
-    let expected = { a: 99, z: 1, b: 4 }
-    expect(m).to.be.eql(expected)
-  })
-})
